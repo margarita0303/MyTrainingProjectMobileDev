b6b05b6f3b626d7ef746d38be9cb656f887d2918
Margarita <lashina.2000@gmail.com>
2022-02-21 18:05:22 +0300

    added video to screen, viewpager element and login/register buttons

--- app/src/main/java/com/margaritalashina/mytrainingprojectmobiledev/OnboardingFragment.kt
+++ app/src/main/java/com/margaritalashina/mytrainingprojectmobiledev/OnboardingFragment.kt
@@ -2,27 +2,47 @@ package com.margaritalashina.mytrainingprojectmobiledev
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.margaritalashina.mytrainingprojectmobiledev.databinding.FragmentOnboardingBinding
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
 
-class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
+class OnboardingFragment : BaseFragment(R.layout.fragment_onboarding) {
 
     private val viewBinding by viewBinding(FragmentOnboardingBinding::bind)
     private var player: ExoPlayer? = null
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         player = SimpleExoPlayer.Builder(requireContext()).build().apply {
-            addMediaItem(MediaItem.fromUri("assets:///onboarding.mp4"))
+            addMediaItem(MediaItem.fromUri("asset:///onboarding.mp4"))
             repeatMode = Player.REPEAT_MODE_ALL
             prepare()
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewBinding.playerView.player = player
+        // произойдет установка адаптера во viewPager
+        viewBinding.viewPager.setTextPages()
+        viewBinding.viewPager.attachDots(viewBinding.onboaringTextTabLayout)
+        // setOnClickListener позволяет установить лямбду, которая будет вызвана, когда пользователь нажимает на кнопку
+        viewBinding.signInButton.setOnClickListener {
+            // TODO
+        }
+
+        viewBinding.signUpButton.setOnClickListener {
+//         // TODO
+        }
     }
 
     override fun onResume() {
@@ -32,11 +52,27 @@ class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
 
     override fun onPause() {
         super.onPause()
-        player?.play()
+        player?.pause()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        player?.play()
+        player?.release()
+    }
+
+    private fun ViewPager2.setTextPages() {
+        adapter =
+            ListDelegationAdapter(onboardingTextAdapterDelegate()).apply {
+                items =
+                    listOf(
+                        getString(R.string.onboarding_view_pager_text_1),
+                        getString(R.string.onboarding_view_pager_text_2),
+                        getString(R.string.onboarding_view_pager_text_3)
+                    )
+            }
+    }
+
+    private fun ViewPager2.attachDots(tabLayout: TabLayout) {
+        TabLayoutMediator(tabLayout, this) { _, _ -> }.attach()
     }
 }
