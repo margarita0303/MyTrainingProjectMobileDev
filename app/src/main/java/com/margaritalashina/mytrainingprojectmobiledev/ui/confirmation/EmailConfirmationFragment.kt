4b76c4e43ee02531870e6331372255d82a3fcdf9
Margarita <lashina.2000@gmail.com>
2022-02-23 03:01:41 +0300

    added e-mail confirmation screen

--- app/src/main/java/com/margaritalashina/mytrainingprojectmobiledev/ui/confirmation/EmailConfirmationFragment.kt
+++ app/src/main/java/com/margaritalashina/mytrainingprojectmobiledev/ui/confirmation/EmailConfirmationFragment.kt
@@ -1,6 +1,9 @@
 package com.margaritalashina.mytrainingprojectmobiledev.ui.confirmation
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.margaritalashina.mytrainingprojectmobiledev.ui.base.BaseFragment
 import com.margaritalashina.mytrainingprojectmobiledev.R
@@ -12,4 +15,10 @@ class EmailConfirmationFragment : BaseFragment(R.layout.fragment_email_confirmat
 
     private val viewModel: EmailConfirmationViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
