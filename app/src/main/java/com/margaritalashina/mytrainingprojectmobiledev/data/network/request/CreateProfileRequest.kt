c7ab82fa2af8c0953d998265c07cb42f51990280
Margarita <lashina.2000@gmail.com>
2022-02-24 23:57:18 +0300

    added requests

---
+++ app/src/main/java/com/margaritalashina/mytrainingprojectmobiledev/data/network/request/CreateProfileRequest.kt
@@ -0,0 +1,13 @@
+package com.margaritalashina.mytrainingprojectmobiledev.data.network.request
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class CreateProfileRequest(
+    @Json(name = "user_name") val username: String,
+    @Json(name = "first_name") val firstname: String,
+    @Json(name = "last_name") val lastname: String,
+    @Json(name = "email") val email: String,
+    @Json(name = "password") val password: String
+)
